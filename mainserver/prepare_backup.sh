abce3995b06f163019cdaa390a22abc82cc6eea4
Kage0x3B <moritz.hein@live.de>
2020-09-02 00:25:47 +0200

    Fix mysqldump

--- mainserver/prepare_backup.sh
+++ mainserver/prepare_backup.sh
@@ -4,4 +4,4 @@ source .env
 
 sudo -u www-data /var/www/cloud.syscy.de/occ maintenance:mode --on
 
-mysqldump --single-transaction -h localhost -unextcloud -p $NEXTCLOUD_DB_PW nextcloud > ~/backup/nextclouddb.sql
+mysqldump --single-transaction -h localhost -unextcloud -p$NEXTCLOUD_DB_PW nextcloud > ~/backup/nextclouddb.sql
