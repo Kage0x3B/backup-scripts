c17e9cbad24202f6179190bf77588635428b2e27
Kage0x3B <moritz.hein@live.de>
2020-09-02 00:16:03 +0200

    Add scripts for main server

---
+++ mainserver/finish_backup.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+sudo -u www-data /var/www/cloud.syscy.de/occ maintenance:mode --off
---
+++ mainserver/prepare_backup.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+source .env
+
+sudo -u www-data /var/www/cloud.syscy.de/occ maintenance:mode --on
+
+mysqldump --single-transaction -h localhost -unextcloud -p $NEXTCLOUD_DB_PW nextcloud > ~/backup/nextclouddb.sql
