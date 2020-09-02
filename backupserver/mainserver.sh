586cbc7bfe89548f783e1d94576f919bde67132a
Kage0x3B <moritz.hein@live.de>
2020-09-02 02:34:03 +0200

    Use different rdiff-backup commands instead of one include list

--- backupserver/mainserver.sh
+++ backupserver/mainserver.sh
@@ -3,7 +3,13 @@
 function backup_mainserver() {
 	ssh vs '~/backup-scripts/mainserver/prepare_backup.sh'
 
-	rdiff-backup --include-globbing-filelist mainserver-list vs::/ /mnt/backupdata/mainserver
+	rdiff-backup vs::/var/www/syscy.de /mnt/backupdata/mainserver/syscy.de
+
+	rdiff-backup vs::/var/nextclouddata /mnt/backupdata/mainserver/nextclouddata
+
+	rdiff-backup vs::/var/www/cloud.syscy.de/config /mnt/backupdata/mainserver/nextcloudconfigs
+
+	rdiff-backup vs::/home/moritz/backup/*.tar.xz /mnt/backupdata/mainserver/databasedump
 
 	ssh vs '~/backup-scripts/mainserver/finish_backup.sh'
 }
