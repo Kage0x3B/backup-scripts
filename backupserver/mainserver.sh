335620c52d92b84638855377017765c8521429b7
Kage0x3B <moritz.hein@live.de>
2020-09-04 01:03:23 +0200

    Add more time displays

--- backupserver/mainserver.sh
+++ backupserver/mainserver.sh
@@ -1,25 +1,25 @@
 #!/usr/bin/env bash
 
 function backup_mainserver() {
-	echo Updating remote backup scripts
+	echo Updating remote backup scripts - $(date +%d.%m.%Y-%H:%M:%S)
 	ssh vs '~/backup-scripts/update.sh'
 
-	echo Preparing backup
+	echo Preparing backup - $(date +%d.%m.%Y-%H:%M:%S)
 	ssh vs '~/backup-scripts/mainserver/prepare_backup.sh'
 
-	echo Backing up syscy.de files
+	echo Backing up syscy.de files - $(date +%d.%m.%Y-%H:%M:%S)
 	rdiff-backup -v4 vs::/var/www/syscy.de /mnt/backupdata/mainserver/syscy.de
 
-	echo Backing up NextCloud data
+	echo Backing up NextCloud data - $(date +%d.%m.%Y-%H:%M:%S)
 	rdiff-backup -v4 vs::/var/nextclouddata /mnt/backupdata/mainserver/nextclouddata
 
-	echo Backing up NextCloud config
+	echo Backing up NextCloud config - $(date +%d.%m.%Y-%H:%M:%S)
 	rdiff-backup -v4 vs::/var/www/cloud.syscy.de/config /mnt/backupdata/mainserver/nextcloudconfigs
 
-	echo Backing up databases
+	echo Backing up databases - $(date +%d.%m.%Y-%H:%M:%S)
 	rdiff-backup -v4 vs::/home/moritz/backup/*.tar.xz /mnt/backupdata/mainserver/databasedump
 
-	echo Finishing backup
+	echo Finishing backup - $(date +%d.%m.%Y-%H:%M:%S)
 	ssh vs '~/backup-scripts/mainserver/finish_backup.sh'
-	echo Done!
+	echo Done! - $(date +%d.%m.%Y-%H:%M:%S)
 }
