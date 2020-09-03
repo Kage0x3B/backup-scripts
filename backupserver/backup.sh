e14d01ff61c3dabf8c1bd255c97360d292fabfed
Kage0x3B <moritz.hein@live.de>
2020-09-04 01:02:11 +0200

    Display start and end times and increase precision to seconds

--- backupserver/backup.sh
+++ backupserver/backup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-echo Starting backups - $(date +%d-%m-%Y)
+echo Starting backups - $(date +%d.%m.%Y-%H:%M:%S)
 
 source mainserver.sh
 #source apserver.sh
@@ -8,4 +8,4 @@ source mainserver.sh
 backup_mainserver
 #backup_apserver
 
-echo Finished backups
+echo Finished backups - $(date +%d.%m.%Y-%H:%M:%S)
