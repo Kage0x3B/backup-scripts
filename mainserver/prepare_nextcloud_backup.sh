3ee36dac39605ebd1142c2b5848ee21f09cc688e
Kage0x3B <moritz.hein@live.de>
2020-09-04 23:12:35 +0200

    Remove previous file to maybe make problems with mysql dump more obvious instead of silently backing up old dump file

--- mainserver/prepare_nextcloud_backup.sh
+++ mainserver/prepare_nextcloud_backup.sh
@@ -8,6 +8,9 @@ sudo -u www-data /var/www/cloud.syscy.de/occ maintenance:mode --on
 
 cd ~/backup
 
+rm -f nextclouddb.sql
+rm -f nextclouddb.tar.xz
+
 mysqldump --single-transaction -h localhost -unextcloud -p$NEXTCLOUD_DB_PW nextcloud > nextclouddb.sql
 
 tar cJf nextclouddb.tar.xz nextclouddb.sql
