43b2b6bbb3e34f7e699d673bd008afb8db677386
Kage0x3B <moritz.hein@live.de>
2020-09-04 23:12:03 +0200

    Script names changed and using scp for backing up database dump

--- backupserver/mainserver.sh
+++ backupserver/mainserver.sh
@@ -4,11 +4,14 @@ function backup_mainserver() {
 	echo Updating remote backup scripts - $(date +%d.%m.%Y-%H:%M:%S)
 	ssh vs '~/backup-scripts/update.sh'
 
-	echo Preparing backup - $(date +%d.%m.%Y-%H:%M:%S)
-	ssh vs '~/backup-scripts/mainserver/prepare_backup.sh'
-
 	echo Backing up syscy.de files - $(date +%d.%m.%Y-%H:%M:%S)
 	rdiff-backup -v4 vs::/var/www/syscy.de /mnt/backupdata/mainserver/syscy.de
+
+	echo Preparing NextCloud backup - $(date +%d.%m.%Y-%H:%M:%S)
+	ssh vs '~/backup-scripts/mainserver/prepare_nextcloud_backup.sh'
+
+	echo Backing up databases - $(date +%d.%m.%Y-%H:%M:%S)
+	scp vs:/home/moritz/backup/nextclouddb.tar.xz /mnt/backupdata/mainserver/databasedump/nextclouddb-$(date +%w).tar.xz
 
 	echo Backing up NextCloud data - $(date +%d.%m.%Y-%H:%M:%S)
 	rdiff-backup -v4 vs::/var/nextclouddata /mnt/backupdata/mainserver/nextclouddata
@@ -16,10 +19,7 @@ function backup_mainserver() {
 	echo Backing up NextCloud config - $(date +%d.%m.%Y-%H:%M:%S)
 	rdiff-backup -v4 vs::/var/www/cloud.syscy.de/config /mnt/backupdata/mainserver/nextcloudconfigs
 
-	echo Backing up databases - $(date +%d.%m.%Y-%H:%M:%S)
-	rdiff-backup -v4 vs::/home/moritz/backup/*.tar.xz /mnt/backupdata/mainserver/databasedump
-
-	echo Finishing backup - $(date +%d.%m.%Y-%H:%M:%S)
+	echo Finishing NextCloud backup - $(date +%d.%m.%Y-%H:%M:%S)
 	ssh vs '~/backup-scripts/mainserver/finish_backup.sh'
 	echo Done! - $(date +%d.%m.%Y-%H:%M:%S)
 }
